fcb1c59713a7a6c65ef23951ed22aa2e408b7fbf
ST10337933 <128132314+ST10337933@users.noreply.github.com>
2023-09-28 19:46:43 +0200

    Add files via upload

---
+++ ST10337933-IMAD5112-PART 2/ST10337933CALCULATORPART1/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ST10337933-CALCULATOR-PART 1"
+include(":app")
+ 
